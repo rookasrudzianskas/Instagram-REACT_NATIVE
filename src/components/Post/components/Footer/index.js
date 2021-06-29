c7c996595607e461ab68ba67c09f088bdf0a4464
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 15:14:16 +0300

    Rokas

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Text, View} from "react-native";
 import styles from "./styles";
-import {SearchOutlined} from "@material-ui/icons";
 
 const Footer = ({ likesCount, caption, postedAt }) => {
     return (
--- src/components/Post/components/Header/index.js
+++ src/components/Post/components/Header/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Text, View} from "react-native";
 import ProfilePicture from "../../../ProfilePicture";
 import styles from "./styles";
+import {MoreVert} from "@material-ui/icons";
 
 const Header = ({ imageURI, name }) => {
     return (
         <View style={styles.container}>
             <ProfilePicture uri={imageURI} size={40} />
             <Text style={styles.name}>{name}</Text>
+            <MoreVert />
         </View>
     );
 };
