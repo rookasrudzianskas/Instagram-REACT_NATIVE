8b71afb3e1870ba4aaf71f2d67abe86a1f8945e7
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 15:29:31 +0300

    Rokas

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Text, View} from "react-native";
 import styles from "./styles";
 import {AddOutlined} from "@material-ui/icons";
+import { AntDesign } from '@expo/vector-icons';
 
 const Footer = ({ likesCount, caption, postedAt }) => {
     return (
         <View style={styles.container}>
+            <View>
+                <AntDesign name="hearto" size={24} color="black" />
+
+            </View>
             <Text style={styles.likes}>{likesCount} likes</Text>
             <Text style={styles.caption}>{caption}</Text>
             <Text style={styles.postedAt}>{postedAt}</Text>
