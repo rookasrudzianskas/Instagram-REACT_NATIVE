5c2e5298b0ba942cb10722a457ceca40d3c3a845
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 13:02:24 +0300

    Roaks

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -6,7 +6,7 @@ const HomeScreen = () => {
    return (
       <>
          <Text>Hello World 🚀</Text>
-         <ProfilePicture />
+         <ProfilePicture uri="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg" />
       </>
 )
 }
