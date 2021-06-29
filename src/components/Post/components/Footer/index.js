9bd2fe4e1a8891f41fec05f3765986deae236ea0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 15:33:17 +0300

    Rokas

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -4,6 +4,7 @@ import styles from "./styles";
 import {AddOutlined} from "@material-ui/icons";
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Footer = ({ likesCount, caption, postedAt }) => {
     return (
@@ -11,6 +12,8 @@ const Footer = ({ likesCount, caption, postedAt }) => {
             <View>
                 <AntDesign name="hearto" size={24}/>
                 <FontAwesome name="comments-o" size={24} />
+                <Ionicons name="ios-paper-plane-outline" size={24} />
+
             </View>
             <Text style={styles.likes}>{likesCount} likes</Text>
             <Text style={styles.caption}>{caption}</Text>
