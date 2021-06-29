41a184b6f8eefd2ab455d2fffcb7ace9b0e826c8
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 21:48:22 +0300

    Rokas

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
           <StatusBar barStyle="dark-content" />
-          <SafeAreaView>
-            <HomeScreen />
-          </SafeAreaView>
+          <Tab.Navigator>
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Settings" component={HomeScreen} />
+          </Tab.Navigator>
     </NavigationContainer>
   );
 }
