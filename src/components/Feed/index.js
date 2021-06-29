4afbf35af8259cc5e3980a6193e2686b61db8353
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 14:49:24 +0300

    Rokas

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import {View} from "react-native";
+import { FlatList } from "react-native";
+import Post from "../Post";
 
+const data = [
+
+    {
+        user: {
+            imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+            name: "Rokas"
+        },
+        imageURI: 'https://i.pinimg.com/originals/89/98/16/899816edb81459f39a5604ae711eae43.jpg',
+        caption: "Amazing City #instagram",
+        likesCount: 1234,
+        postedAt: '6 minutes ago',
+    },
+
+    {
+        user: {
+            imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+            name: "Rokas"
+        },
+        imageURI: 'https://images.megapixl.com/4486/44864763.jpg',
+        caption: "Amazing City #instagram",
+        likesCount: 1234,
+        postedAt: '6 minutes ago',
+    },
+
+    {
+        user: {
+            imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+            name: "Rokas"
+        },
+        imageURI: 'https://samseesworld.com/wp-content/uploads/2020/12/beach-captions-for-instagram.jpg',
+        caption: "Amazing City #instagram",
+        likesCount: 1234,
+        postedAt: '6 minutes ago',
+    },
+
+]
 const Feed = () => {
     return (
-        <View>
-
-        </View>
+        // renders the list of photos
+        <FlatList data={data} renderItem={({item}) => <Post post={item} />} />
     );
 };
 
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import {Text} from "react-native";
 import Stories from "../../components/Stories";
-import Post from "../../components/Post";
+import Feed from "../../components/Feed";
 
-const post = {
-    user: {
-        imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
-        name: "Rokas"
-    },
-    imageURI: 'https://i.pinimg.com/originals/89/98/16/899816edb81459f39a5604ae711eae43.jpg',
-    caption: "Amazing City #instagram",
-    likesCount: 1234,
-    postedAt: '6 minutes ago',
-}
 
 const HomeScreen = () => {
    return (
       <>
          <Stories />
-          <Post post={post} imageURI={post.imageURI} caption={post.caption} likesCount={post.likesCount} postedAt={post.postedAt} />
+          <Feed/>
       </>
 )
 }
