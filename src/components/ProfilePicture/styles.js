87051adb4e735462187a663048e00dc79a44084d
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 12:53:10 +0300

    Roaks

--- src/components/ProfilePicture/styles.js
+++ src/components/ProfilePicture/styles.js
@@ -1,5 +1,11 @@
 import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
-
+    image: {
+        height: 80,
+        width: 80,
+        borderRadius: 50,
+    }
 });
+
+export default styles;
