b502037841c050dd1b75616c6a2caeac43d142f8
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 13:03:24 +0300

    Rokas

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -7,6 +7,9 @@ const HomeScreen = () => {
       <>
          <Text>Hello World 🚀</Text>
          <ProfilePicture uri="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg" />
+         <ProfilePicture uri="https://randomuser.me/api/portraits/men/18.jpg" />
+         <ProfilePicture uri="https://randomuser.me/api/portraits/men/28.jpg" />
+         <ProfilePicture uri="https://randomuser.me/api/portraits/men/29.jpg" />
       </>
 )
 }
