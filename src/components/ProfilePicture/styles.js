908692d415bf738a6cef26c49194645667d6a264
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 13:21:23 +0300

    Rokas

--- src/components/ProfilePicture/styles.js
+++ src/components/ProfilePicture/styles.js
@@ -2,16 +2,16 @@ import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        height: 86,
-        width: 86,
+        height: 66,
+        width: 66,
         margin: 10,
         borderRadius: 40,
         borderWidth: 3,
         borderColor: "#ae22e0",
     },
     image: {
-        height: 80,
-        width: 80,
+        height: 60,
+        width: 60,
         borderRadius: 40,
         borderWidth: 1,
         borderColor: "#fff",
--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -23,12 +23,32 @@ const data = [
         name: 'Tim',
     },
 
+    {
+        imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+        name: 'Bronson',
+    },
+
+    {
+        imageURI: 'https://randomuser.me/api/portraits/men/18.jpg',
+        name: 'Naz',
+    },
+
+    {
+        imageURI: 'https://randomuser.me/api/portraits/men/28.jpg',
+        name: 'Quazi',
+    },
+
+    {
+        imageURI: 'https://randomuser.me/api/portraits/men/29.jpg',
+        name: 'Jamie',
+    },
+
 
 
 ]
 const Stories = () => {
     return (
-        <FlatList keyExtractor={({name}) => name} orientation={'horizontal'} data={data} renderItem={({item}) => <Story imageURI={item.imageURI} name={item.name} />} />
+        <FlatList keyExtractor={({name}) => name} horizontal data={data} renderItem={({item}) => <Story imageURI={item.imageURI} name={item.name} />} />
     )
 }
 
