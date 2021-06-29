7ba10cb35f5e2250c49512fe3cbb0be5d04636a0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 12:53:40 +0300

    Roaks

--- src/components/ProfilePicture/index.js
+++ src/components/ProfilePicture/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Image} from "react-native";
+import {Image, View} from "react-native";
 import styles from "./styles";
 
 const ProfilePicture = () => {
     return (
-        <>
+        <View>
             <Image style={styles.image} source={{ uri: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg'}} />
-        </>
+        </View>
         )
 }
 
