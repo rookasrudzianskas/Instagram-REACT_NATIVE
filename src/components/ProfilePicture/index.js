a2b464e07195b1ad7e38789e4209a3a412aae215
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 12:49:39 +0300

    Roaks

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Image} from "react-native";
+
+const ProfilePicture = () => {
+    return (
+        <>
+            <Image source={{uri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/p160x160/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=dbb9e7&_nc_eui2=AeGjZbecmqwLvnlbx3STRUcixlokCBJZ6JPGWiQIElnok5b36VQxmkbd_xA74WOh5Md7TWZNOQ6TSiAdKeQf8RJ9&_nc_ohc=tq7CYqHvRYcAX_LwjFy&_nc_ht=scontent.fkiv3-1.fna&_nc_tp=6&oh=a7fda4462bbac7dcca956ac0c492e739&oe=5F40895B'}} />
+        </>
+        )
+}
+
+export default ProfilePicture;
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import {Text} from "react-native";
+import ProfilePicture from "../../components/ProfilePicture";
 
 const HomeScreen = () => {
-   return <Text>Hello World 🚀</Text>
+   return (
+      <>
+         <Text>Hello World 🚀</Text>
+         <ProfilePicture />
+      </>
+)
 }
 
 export default HomeScreen;
