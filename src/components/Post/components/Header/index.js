737bcace6862debc8864dd9c9cb08cb3fbdcf32b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-29 14:08:11 +0300

    Rokas

--- src/components/Post/components/Header/index.js
+++ src/components/Post/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from "react-native";
 import ProfilePicture from "../../../ProfilePicture";
+import styles from "./styles";
 
 const Header = ({ imageURI, name }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <ProfilePicture uri={imageURI} size={40} />
             <Text>{name}</Text>
         </View>
