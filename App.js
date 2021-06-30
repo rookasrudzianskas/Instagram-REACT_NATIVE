3011d99a488eb678baa8db8ef152609ac237a197
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 07:55:29 +0300

    ROkas

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
 import DiscoveryScreen from "./src/screens/DiscoveryScreen";
 import CreatePostScreen from "./src/screens/CreatePostScreen";
@@ -9,6 +9,7 @@ import ProfileScreen from "./src/screens/ProfileScreen";
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
