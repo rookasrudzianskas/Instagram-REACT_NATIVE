bcc86142085378b0b68c1524d0e649ebf3fba9b0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 08:52:59 +0300

    ROkas

--- src/components/Stories/styles.js
+++ src/components/Stories/styles.js
@@ -3,6 +3,7 @@ import {StyleSheet} from "react-native";
 const styles = StyleSheet.create({
     container: {
         marginBottom: 15,
+        marginTop: 8,
     }
 });
 
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from "react-native";
+
+const Router = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default Router;
