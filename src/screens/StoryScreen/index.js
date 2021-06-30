ac4a4dbc579a8c53916bdb4cf65df2cf51f97f43
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 10:04:23 +0300

    Rokas

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, SafeAreaView} from "react-native";
 import {useRoute} from "@react-navigation/native";
 import storiesData from "../../data/stories.js";
 
 const StoryScreen = () => {
 
+    const [userStoriesPreview, setUserStoriesPreview] = useState(null);
+    const [activeStoryIndex, setActiveStoryIndex] = useState(null);
+    const [activeStory, setActiveStory] = useState(null);
+
+
     const route = useRoute();
 
-    const userId = route.params.userId;
 
-    const userStories = storiesData.find(storiesData => storiesData.user.id === userId);
-    console.log("userStories");
-    console.log(userStories);
+    useEffect(() => {
+        const userId = route.params.userId;
+        const userStories = storiesData.find(storiesData => storiesData.user.id === userId);
+
+        setUserStoriesPreview(userStories);
+        setActiveStoryIndex(0);
+    }, []);
+
+    useEffect(() =>  {
+        setActiveStory(userStoriesPreview.stories[activeStoryIndex]);
+    }, [])
 
 
     return (
