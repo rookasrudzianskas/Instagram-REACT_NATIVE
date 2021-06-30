8ba2024133925a58ab7d8f0c4a8f292d19ad0e96
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 09:15:38 +0300

    ROkas

--- src/components/UserStoryPreview/index.js
+++ src/components/UserStoryPreview/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, TouchableHighlight, View} from "react-native";
 import ProfilePicture from "../ProfilePicture";
 import styles from "./style"
 
 const Story = ({ imageURI, name }) => {
+
+    const onPress = () => {
+        console.log("User Story Pressed");
+    }
     return (
-        <View>
+        <TouchableHighlight style={styles.container} onPress={onPress}>
             <ProfilePicture uri={imageURI} />
             <Text style={styles.name}>{name}</Text>
-        </View>
+        </TouchableHighlight>
     )
 }
 
