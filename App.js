5594e6c157432ebf68ada86007d8d88dc5ce261e
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 07:52:54 +0300

    ROkas

--- App.js
+++ App.js
@@ -16,7 +16,27 @@ export default function App() {
   return (
       <NavigationContainer>
           <StatusBar barStyle="dark-content" />
-          <Tab.Navigator>
+          <Tab.Navigator
+              screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+
+                  if (route.name === 'Home') {
+                      iconName = focused
+                          ? 'ios-information-circle'
+                          : 'ios-information-circle-outline';
+                  } else if (route.name === 'Settings') {
+                      iconName = focused ? 'ios-list-box' : 'ios-list';
+                  }
+
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />;
+              },
+          })}
+                         tabBarOptions={{
+                             activeTintColor: 'tomato',
+                             inactiveTintColor: 'gray',
+                         }}>
               {/* Navigation*/}
               <Tab.Screen name="Home" component={HomeScreen} />
               <Tab.Screen name="Discovery" component={DiscoveryScreen} />
