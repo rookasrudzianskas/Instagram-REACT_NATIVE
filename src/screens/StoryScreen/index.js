727d65c3d1ac3627ce5ab5230e5901d791cdcfeb
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 09:23:07 +0300

    ROkas

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, SafeAreaView} from "react-native";
 
 const StoryScreen = () => {
     return (
-        <Text>
-            Rokas
-        </Text>
+        <SafeAreaView>
+            <Text>Rokas</Text>
+        </SafeAreaView>
     );
 };
 
