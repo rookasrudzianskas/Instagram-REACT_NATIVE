e92682be018c25cc82ba53be5379ebaed68c6288
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 14:41:33 +0300

    Rokas

--- src/components/UserStoriesPreview/index.js
+++ src/components/UserStoriesPreview/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Story from "../UserStoryPreview";
 import {View, FlatList} from "react-native";
 import styles from "./styles";
 import storiesData from "../../data/stories";
 import {API, graphqlOperation} from "aws-amplify";
-
+import {listStorys} from "../../graphql/queries";
 
 const Stories = () => {
+
+    const [stories, setStories] = useState([]);
+
+    useEffect(() => {
+        fetchStories();
+    }, []);
+
+    const fetchStories = () => {
+
+    }
+
     return (
         <FlatList style={styles.container} showsHorizontalScrollIndicator={false} keyExtractor={({user: {id}}) => id} horizontal data={storiesData} renderItem={({item}) => <Story story={item} />} />
     )
