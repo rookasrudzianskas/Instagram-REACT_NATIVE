aad28d8928b168ee45101cf12d95e98d7b4b0864
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 09:43:33 +0300

    Rokas

--- src/data/stories.js
+++ src/data/stories.js
@@ -1,42 +1,178 @@
 export default [
     {
-        imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
-        name: 'Rokas',
+        user: {
+            imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+            name: 'Rokas',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/18.jpg',
-        name: 'John',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/18.jpg',
+            name: 'John',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/28.jpg',
-        name: 'James',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/28.jpg',
+            name: 'James',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/29.jpg',
-        name: 'Tim',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/29.jpg',
+            name: 'Tim',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
-        name: 'Bronson',
+        user: {
+            imageURI: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg',
+            name: 'Bronson',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/18.jpg',
-        name: 'Naz',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/18.jpg',
+            name: 'Naz',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/28.jpg',
-        name: 'Quazi',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/28.jpg',
+            name: 'Quazi',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
     {
-        imageURI: 'https://randomuser.me/api/portraits/men/29.jpg',
-        name: 'Jamie',
+        user: {
+            imageURI: 'https://randomuser.me/api/portraits/men/29.jpg',
+            name: 'Jamie',
+
+        },
+        stories: [
+            {
+                imageUri: "https://i.insider.com/6082fa3d4becb800190f6dd5?width=600&format=jpeg&auto=webp",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://cdn.guidingtech.com/imager/assets/202593/color-block-instagram-story-3_7c4a12eb7455b3a1ce1ef1cadcf29289.png?1535213783",
+                postedTime: "25 minutes",
+            },
+            {
+                imageUri: "https://i.pinimg.com/originals/4b/d4/e7/4bd4e7e02b994b22b6d339581adada69.jpg",
+                postedTime: "25 minutes",
+            },
+        ]
     },
 
 
